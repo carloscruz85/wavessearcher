6905b868e832286a666ee7420cfe047e734056dc
carloscruz85 <>
2021-06-11 11:05:46 -0600

    PWA settings is done

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -2,6 +2,8 @@ import React, { useEffect, useContext } from "react";
 import Logo from '../../assets/images/logo-surf-city.png'
 import {DataContext} from '../../context/context'
 import List from '../list'
+import './index.scss'
+import Surf from '../../assets/images/surf.jpg'
 
 const App = () => {
 
@@ -16,19 +18,22 @@ const App = () => {
   return (
     <>
       {globalData.filtered.length === 0 || globalData.filtered.length === undefined ?
-      <div className="container" >
+      <div className="container intro" >
         <div className="logo-container">
           <img src={Logo} alt="Surf City" className="logo"/>
         </div>        
-        <div className="intro basic-p" style={{marginTop: '1rem'}}>
+        <img src={Surf} alt="Surf" className="liquid"/>
+        <div className=" basic-p" style={{marginTop: '1rem'}}>
           {
            lang === 'es' ? (<div>
-             <h3>Bienvenido al Buscador de Olas</h3>
+              <div className="intro-title"> Bienvenido </div>
+              <div>Al Buscador de Olas </div>
             <p>Las playas de El Salvador son el lugar perfecto para hacer surf 🏄. <br />Gracias a nuestra privilegiada geografía contamos con los mejores spots para los amantes de las olas 🤙</p>
             <p>En este buscador hemos agrupado nuestras playas según las clasificaciones que los expertos usan, como el tipo de ola, dirección, características de la playa etc, estos criterios los encontrarás en el menú de la izquierda</p>
           </div>) : (
             <div>
-              <h3> Welcome to the Wave Searcher </h3>
+              <div className="intro-title"> Welcome </div>
+              <div>to the Wave Searcher </div>
              <p> The beaches of El Salvador are the perfect place to surf 🏄. <br /> Thanks to our privileged geography we have the best spots for wave lovers 🤙 </p>
              <p> In this search we have grouped our beaches according to the classifications that the experts use, such as the type of wave, direction, type of the beach, etc., these criteria can be found in the menu on the left </p>
             </div>
